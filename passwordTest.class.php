0e2a2bdb1a932eb4795e274b62fdcfccdf243b57
MickaelBenes <mickaelbenes@hotmail.fr>
2017-02-10 00:15:32 +0100

    Algo upgrade.
    Upgrade of the algo that checks if the password countains digits, lower, upper and special chars.

--- passwordTest.class.php
+++ passwordTest.class.php
@@ -1,10 +1,10 @@
 class PasswordTest {
-
-	const PWDSTR_JOKE		= 1;
+	
+	const PWDSTR_JOKE	= 1;
 	const PWDSTR_VERYWEAK	= 2;
-	const PWDSTR_WEAK		= 3;
-	const PWDSTR_MEDIUM		= 4;
-	const PWDSTR_STRONG		= 5;
+	const PWDSTR_WEAK	= 3;
+	const PWDSTR_MEDIUM	= 4;
+	const PWDSTR_STRONG	= 5;
 	const PWDSTR_VERYSTRONG	= 6;
 	
 	private $_pwd;
@@ -12,40 +12,39 @@ class PasswordTest {
 	public function __construct( $sPassword ) {
 		$this->_pwd = $sPassword;
 	}
-
+	
 	public function getPasswordStrength() {
 		$bHasNumeric	= false;
-		$bHasLower		= false;
-		$bHasUpper		= false;
+		$bHasLower	= false;
+		$bHasUpper	= false;
 		$bHasSpecials	= false;
 		
-		for ( $i = 0; $i < strlen($this->_pwd); $i++ ) {
-			$char = $this->_pwd[ $i ];
-			
-			if ( is_numeric($char) ) {
-				$bHasNumeric = true;
-			}
-			else if ( ctype_lower($char) ) {
-				$bHasLower = true;
-			}
-			else if ( ctype_upper($char) ) {
-				$bHasUpper = true;
-			}
-			else if ( preg_match('¤\W¤', $char) ) {
-				$bHasSpecials = true;
-			}
+		if ( preg_match('¤\d¤', $this->_pwd) ) {
+			$bHasNumeric = true;
 		}
-
+		
+		if ( preg_match('¤[a-z]¤', $this->_pwd) ) {
+			$bHasLower = true;
+		}
+		
+		if ( preg_match('¤[A-Z]¤', $this->_pwd) ) {
+			$bHasUpper = true;
+		}
+		
+		if ( preg_match('¤\W¤', $this->_pwd) ) {
+			$bHasSpecials = true;
+		}
+		
 		// contains digits and lowercase and uppercase letters and special characters
 		if ( $bHasNumeric && $bHasLower && $bHasUpper && $bHasSpecials ) {
 			return self::PWDSTR_VERYSTRONG;
 		}
-
+		
 		// contains digits and lowercase and uppercase letters
 		if ( $bHasNumeric && $bHasLower && $bHasUpper ) {
 			return self::PWDSTR_STRONG;
 		}
-
+		
 		// contains both lowercase and uppercase letters
 		if ( !$bHasNumeric && $bHasLower && $bHasUpper ) {
 			return self::PWDSTR_MEDIUM;
@@ -55,12 +54,12 @@ class PasswordTest {
 		if ( $bHasNumeric && (($bHasLower && !$bHasUpper) || (!$bHasLower && $bHasUpper)) ) {
 			return self::PWDSTR_WEAK;
 		}
-
+		
 		// contains only letters (uppercase or lowercase)
 		if ( ctype_lower($this->_pwd) || ctype_upper($this->_pwd) ) {
 			return self::PWDSTR_VERYWEAK;
 		}
-
+		
 		// contains only digits
 		if ( is_numeric($this->_pwd) ) {
 			return self::PWDSTR_JOKE;
